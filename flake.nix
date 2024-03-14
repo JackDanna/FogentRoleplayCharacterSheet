4a8193fca947557aa50e4f9db7d28b23c52cfd6e
JackDanna <jackdanna78@gmail.com>
2024-03-14 10:37:46 -0400

    Fixed flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,65 @@
+
 {
-  description = "A very basic flake";
+  description = "A fun enviroment to test FSharp";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
+          "vscode-with-extensions"
+          "vscode"
+        ];
+      };
+    };
+  in 
+  {
+    packages.${system} = {
 
-  outputs = { self, nixpkgs }: {
+      default = pkgs.writeShellScriptBin "run" ''
+        nix develop -c -- code .
+      '';
+      
+      test = pkgs.writeShellScriptBin "fa" ''
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        ${pkgs.figlet}/bin/figlet "Fallen is awesome!"
+      '';
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    };
+    
 
-  };
+    devShells.${system}.default = pkgs.mkShell rec {
+      name = "FSharpFun";
+      buildInputs = with pkgs; [
+        gnome.gnome-terminal
+        bashInteractive
+        dotnet-sdk_8
+        (vscode-with-extensions.override  {
+          vscode = pkgs.vscode;
+          vscodeExtensions = with pkgs.vscode-extensions; [
+            jnoortheen.nix-ide
+            mhutchie.git-graph
+            ms-dotnettools.csharp
+            ionide.ionide-fsharp
+          ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "vscode-edit-csv";
+              publisher = "janisdd";
+              version = "0.8.2";
+              sha256 = "sha256-DbAGQnizAzvpITtPwG4BHflUwBUrmOWCO7hRDOr/YWQ=";
+            }
+          ];
+        })
+      ];
+
+      shellHook = ''
+        export PS1+="${name}> "
+        echo "Welcome to the FSharp Fun"
+      '';
+    };
+  }; 
+
 }
+
