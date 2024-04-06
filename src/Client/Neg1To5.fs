72c85cffe6fa9c046a9cc5c165a017741eaf5828
JackDanna <jackdanna78@gmail.com>
2024-04-06 11:38:29 -0400

    Added Neg1To5

---
+++ src/Client/Neg1To5.fs
@@ -0,0 +1,28 @@
+module Neg1To5
+
+open FogentRoleplayLib.Neg1To5
+
+type Model = Neg1To5
+
+type Msg =
+    | ChangedNeg1To5 of int
+
+let init () = Zero
+
+let update (msg: Msg) (model: Neg1To5) : Model =
+    match msg with
+    | ChangedNeg1To5 newValue ->
+        match (intToNeg1To5Option newValue) with
+        | Some newNeg2To5 -> newNeg2To5
+        | None -> model
+
+open Feliz
+open Feliz.Bulma
+
+let view (model: Model) dispatch =
+    Bulma.input.number  [
+        prop.max 5
+        prop.min -1
+        prop.value (neg1To5ToInt model)
+        prop.onChange ( fun num -> dispatch (ChangedNeg1To5 (num)) )
+    ]
--- src/Shared/FogentRoleplayLib.fs
+++ src/Shared/FogentRoleplayLib.fs
@@ -45,3 +45,34 @@ module AttributeStat =
         attribute: Attribute
         stat: Neg2To5
     }
+
+module Neg1To5 =
+    type Neg1To5 =
+        | NegOne
+        | Zero
+        | One
+        | Two
+        | Three
+        | Four
+        | Five
+
+    let intToNeg1To5Option num =
+        match num with
+        | -1 -> Some NegOne
+        | 0 -> Some Zero
+        | 1 -> Some One
+        | 2 -> Some Two
+        | 3 -> Some Three
+        | 4 -> Some Four
+        | 5 -> Some Five
+        | _ -> None
+    
+    let neg1To5ToInt neg1To5 =
+        match neg1To5 with
+        | NegOne -> -1
+        | Zero -> 0
+        | One -> 1
+        | Two -> 2
+        | Three -> 3
+        | Four -> 4
+        | Five -> 5
