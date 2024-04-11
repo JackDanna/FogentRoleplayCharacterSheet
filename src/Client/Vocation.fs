af4d27446178e2bb7e3a3e35b239dea80e99bd86
JackDanna <jackdanna78@gmail.com>
2024-04-11 16:10:38 -0400

    Fixed Vocation Skill list

---
+++ src/Client/Vocation.fs
@@ -0,0 +1,6 @@
+module vocationList
+
+open FogentRoleplayLib.Vocation
+
+type Msg =
+    | ModifiedVocationAtPostion of int of Vocation
---
+++ src/Client/VocationSkillList.fs
@@ -0,0 +1,41 @@
+module VocationSkillList
+
+open FogentRoleplayLib.Skill
+open FogentRoleplayLib.VocationSkill
+
+type Msg =
+    | ModifiedVocationSkillAtPosition of int * VocationSkill.Msg
+    | CalculateDicePools of DicePoolCalculationData
+
+// let init () =
+
+let update msg model =
+    match msg with
+    | ModifiedVocationSkillAtPosition(position, msg) ->
+        model
+        |> List.mapi (fun index vocationSkill ->
+            if index = position then
+                VocationalSkill.update msg vocationSkill
+            else
+                vocationSkill)
+    | CalculateDicePools dicePoolCalculationData ->
+        List.map
+            (fun vocationSkill ->
+                VocationalSkill.update (VocationalSkill.CalculateDicePool(dicePoolCalculationData)) vocationSkill)
+            model
+
+open Feliz
+open Feliz.Bulma
+
+let view (model: VocationSkill list) dispatch =
+    List.append
+        (List.mapi
+            (fun position skillRow ->
+                VocationSkill.view skillRow (fun (msg: VocationSkill.Msg) ->
+                    ((ModifiedVocationSkillAtPosition(position, msg)) |> dispatch)))
+            model)
+        [
+        // Html.button [ prop.onClick (fun _ -> dispatch Insert); prop.text "+" ]
+        // Html.button [ prop.onClick (fun _ -> dispatch Remove); prop.text "-" ]
+        ]
+    |> Html.ul
