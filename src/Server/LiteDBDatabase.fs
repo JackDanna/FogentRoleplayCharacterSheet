c23127fb2f0c8b09975d875aa6f0486491b61454
Jack Danna <jackdanna78@gmail.com>
2024-09-23 11:53:47 -0400

    Added some database logic

--- src/Server/LiteDBDatabase.fs
+++ src/Server/LiteDBDatabase.fs
@@ -15,11 +15,13 @@ let db =
 [<CLIMutable>]
 type IdEntity<'T> = { Id: int; Entity: 'T }
 
-let collectionFromDB<'T> = db.GetCollection<IdEntity<'T>>(typeof<'T>.Name)
+let collectionFromDB<'T> = db.GetCollection<'T>(typeof<'T>.Name)
 
 let insertEntity (entity: 'T) =
     // Since Id is set to 0, inserted entities will be placed according to auto-incrementation
-    collectionFromDB.Insert({ Id = 0; Entity = entity }) |> ignore
+    let idEntity = { Id = 0; Entity = entity }
+    collectionFromDB.Insert(idEntity) |> ignore
+    idEntity
 
 let findEntity entity = collectionFromDB.FindById entity.Id
 
@@ -27,3 +29,53 @@ let insertEntities entities =
     entities |> Seq.map (fun entity -> insertEntity entity)
 
 open CsvDatabase
+
+open Shared
+
+type IdUser = IdEntity<Login>
+type IdCharacter = IdEntity<FogentRoleplayLib.Character.Character>
+
+type UserCharacterAccess = {
+    UserId: int
+    CharacterId: int
+//AccessGranted: System.DateTime
+//AccessType: string
+}
+
+let users = collectionFromDB<IdUser>
+let characters = collectionFromDB<IdCharacter>
+let userCharacterAccesses = collectionFromDB<UserCharacterAccess>
+
+//users.EnsureIndex(fun (u: IdUser) -> u.Entity.userName) |> ignore
+
+let insertNewUser (user: Login) = insertEntity user
+
+let insertCharacter character = insertEntity character
+
+let grantAccess userId characterId =
+    userCharacterAccesses.Insert(
+        {
+            UserId = userId
+            CharacterId = characterId
+        //AccessGrantedDate = System.DateTime.UtcNow
+        //AccessType = accessType
+        }
+    )
+    |> ignore
+
+let addNewCharacter settingData userId =
+    let character = FogentRoleplayLib.Character.init settingData
+    let idCharacter = insertEntity character
+
+    grantAccess userId idCharacter.Id
+
+let getCharactersForUser userId =
+    userCharacterAccesses.Find(fun uca -> uca.UserId = userId)
+    |> Seq.map (fun uca -> characters.FindById(BsonValue(uca.CharacterId)))
+    |> Seq.toArray
+
+// Function to get all users who have access to a character
+let getUsersForCharacter characterId =
+    userCharacterAccesses.Find(fun uca -> uca.CharacterId = characterId)
+    |> Seq.map (fun uca -> users.FindById(BsonValue(uca.UserId)))
+    |> Seq.toArray
